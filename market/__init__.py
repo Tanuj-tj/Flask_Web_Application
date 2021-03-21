da36af747f88f77bfa2da77ff1df0341b4e151d5
Tanuj-tj <shrivastavatanuj5@gmail.com>
2021-03-21 16:40:20 +0530

    New Files Added

--- market/__init__.py
+++ market/__init__.py
@@ -5,11 +5,10 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = '6cb0ef1ad543cb521ecdaaaf'  # <-- Randomly generated
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
-
 from market import routs
