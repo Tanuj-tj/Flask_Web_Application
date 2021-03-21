96c7a796279fda2307ab2c85b25f0b4c9068b334
Tanuj-tj <shrivastavatanuj5@gmail.com>
2021-03-21 17:38:00 +0530

    Code Updated

--- market/__init__.py
+++ market/__init__.py
@@ -11,4 +11,4 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
-from market import routs
+from market import routes
